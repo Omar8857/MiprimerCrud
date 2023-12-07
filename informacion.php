297cb42ec77793407c22f66b28053c5f997bdc5a
Omar8857 <65386307+Omar8857@users.noreply.github.com>
2023-12-06 18:03:53 -0600

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -10,15 +10,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Verificar si el formulario ha sido enviado
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Verificar si los campos específicos están definidos en el array $_POST
+ 
     $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : "";
     $apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : "";
     $edad = isset($_POST['edad']) ? $_POST['edad'] : "";
     $domicilio = isset($_POST['domicilio']) ? $_POST['domicilio'] : "";
 
-    // Validar y escapar los datos del formulario
+    
     $nombre = mysqli_real_escape_string($conn, $nombre);
     $apellidos = mysqli_real_escape_string($conn, $apellidos);
     $edad = mysqli_real_escape_string($conn, $edad);
